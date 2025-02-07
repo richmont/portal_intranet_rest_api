656b73765cd3222c4b65b6980076d3a08c0307e9
Richelmy TI <richelmy.ti@formosanet.com.br>
2025-02-07 13:58:22 -0300

    Remoção de email único para permitir email vazio

--- intranet_api/models/Contato.py
+++ intranet_api/models/Contato.py
@@ -4,8 +4,8 @@ from . import Setor,Loja
 
 class Contato(models.Model):
     nome = models.CharField(max_length=60)
-    ramal = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
+    ramal = models.CharField(max_length=30, unique=True)
+    email = models.EmailField()
     setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
     loja = models.ForeignKey(Loja, on_delete=models.CASCADE)
     
