99e9ccd69d56831faa49bfbc0b52ff991fbea0d2
Richelmy TI <richelmy.ti@formosanet.com.br>
2025-02-13 12:10:14 -0300

    Permite email em branco no Contato

--- intranet_api/models/Contato.py
+++ intranet_api/models/Contato.py
@@ -5,7 +5,7 @@ from . import Setor,Loja
 class Contato(models.Model):
     nome = models.CharField(max_length=60)
     ramal = models.CharField(max_length=30, unique=True)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
     loja = models.ForeignKey(Loja, on_delete=models.CASCADE)
     
